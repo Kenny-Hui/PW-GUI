459ca3388e3dc50155eeabd01dedab4e62d59b8f
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-01-25 20:44:58 +0800

    Remove NBT Library
    Don't feel like implementing NBT preview for now :P

--- src/main/java/com/lx862/pwgui/data/fileentry/ServerDatFile.java
+++ src/main/java/com/lx862/pwgui/data/fileentry/ServerDatFile.java
@@ -1,24 +1,11 @@
 package com.lx862.pwgui.data.fileentry;
 
-import net.querz.nbt.io.NBTUtil;
-import net.querz.nbt.io.NamedTag;
-
 import java.io.File;
 
 public class ServerDatFile extends FileSystemEntityEntry {
-    private NamedTag deserializedTag;
 
     public ServerDatFile(File file) {
         super(file);
-        try {
-            this.deserializedTag = NBTUtil.read(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public NamedTag getTag() {
-        return deserializedTag;
     }
 
     @Override

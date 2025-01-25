28ad15285e7f789e0218d73f921c3e0f0da242bd
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-01-26 00:09:16 +0800

    Bump version to 0.9.0

--- src/main/java/com/lx862/pwgui/core/Constants.java
+++ src/main/java/com/lx862/pwgui/core/Constants.java
@@ -2,7 +2,7 @@ package com.lx862.pwgui.core;
 
 public final class Constants {
     public static final String PROGRAM_NAME = "PW-GUI";
-    public static final String VERSION = "0.8.0";
+    public static final String VERSION = "0.9.0";
     public static final String REASON_TRIGGERED_BY_USER = "Requested by user";
 
     public static final String LINK_GITHUB = "https://github.com/Kenny-Hui/PW-GUI";

57216c68e06e769dbb356f88dcae8878108e9b03
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-01-28 02:17:41 +0800

    Fix file not getting watched after renaming

--- src/main/java/com/lx862/pwgui/gui/components/fstree/FileSystemWatcher.java
+++ src/main/java/com/lx862/pwgui/gui/components/fstree/FileSystemWatcher.java
@@ -5,20 +5,24 @@ import com.lx862.pwgui.Main;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.function.BiConsumer;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 
 public class FileSystemWatcher {
     private final Path path;
     private final WatchEvent.Kind<?>[] watchKinds;
     private final boolean recursive;
+    private final HashMap<Path, WatchKey> watchKeys;
     private WatchService ws;
 
     public FileSystemWatcher(Path path, boolean recursive, WatchEvent.Kind<?> ...watchKinds) {
         this.path = path;
         this.recursive = recursive;
         this.watchKinds = watchKinds;
+        this.watchKeys = new HashMap<>();
     }
 
     @SuppressWarnings("InfiniteLoopStatement")
@@ -33,12 +37,12 @@ public class FileSystemWatcher {
                     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                     if(dir.toFile().getName().equals(".git")) return FileVisitResult.SKIP_SUBTREE; // git directory has way too many files, we are not interested in it anyway
 
-                    dir.register(ws, watchKinds);
+                    watchKeys.put(dir, dir.register(ws, watchKinds));
                     return FileVisitResult.CONTINUE;
                     }
                 });
             } else {
-                this.path.register(this.ws, this.watchKinds);
+                watchKeys.put(path, path.register(this.ws, this.watchKinds));
             }
 
             while(true) {
@@ -47,15 +51,23 @@ public class FileSystemWatcher {
 
                 for (WatchEvent<?> e : wk.pollEvents())
                 {
-                    Path directory = (Path)wk.watchable();
                     WatchEvent.Kind<?> kind = e.kind();
+                    Path parentDirectory = (Path)wk.watchable();
+                    Path filePath = parentDirectory.resolve(((WatchEvent<Path>)e).context());
+
                     if(recursive && kind == ENTRY_CREATE) {
-                        final Path filePath = directory.resolve(((WatchEvent<Path>)e).context());
                         if(Files.isDirectory(filePath)) {
-                            filePath.register(ws, watchKinds); // Watch for our new folder
+                            watchKeys.put(filePath, filePath.register(ws, watchKinds)); // Watch for our new folder
                         }
                     }
 
+                    if(kind == ENTRY_DELETE) {
+                        WatchKey existingWatchKey = watchKeys.get(filePath);
+                        if(existingWatchKey != null) {
+                            existingWatchKey.cancel(); // Unregister our old entry
+                            watchKeys.remove(path);
+                        }
+                    }
                     callback.accept(wk, e);
                 }
                 wk.reset();

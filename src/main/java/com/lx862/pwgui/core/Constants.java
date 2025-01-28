8268e41648de62428b489cde33b56bfb25af28c8
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-01-28 19:58:00 +0800

    Give it a little push!
    Bump version to 1.0.0

--- src/main/java/com/lx862/pwgui/core/Constants.java
+++ src/main/java/com/lx862/pwgui/core/Constants.java
@@ -3,6 +3,6 @@ package com.lx862.pwgui.core;
 /** Static values for the program */
 public final class Constants {
     public static final String PROGRAM_NAME = "PW-GUI";
-    public static final String VERSION = "0.9.0";
+    public static final String VERSION = "1.0.0";
     public static final String REASON_TRIGGERED_BY_USER = "Requested by user";
 }

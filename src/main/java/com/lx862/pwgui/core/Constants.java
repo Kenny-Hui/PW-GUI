cb3433cd7512e1484a9d8a9a038b009594c26f2e
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-06-30 03:25:00 +0800

    Bump version

--- src/main/java/com/lx862/pwgui/core/Constants.java
+++ src/main/java/com/lx862/pwgui/core/Constants.java
@@ -3,7 +3,7 @@ package com.lx862.pwgui.core;
 /** Static values for the program */
 public final class Constants {
     public static final String PROGRAM_NAME = "PW-GUI";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     public static final String AUTHOR = "LX86";
     public static final String REASON_TRIGGERED_BY_USER = "Requested by user";
 }
